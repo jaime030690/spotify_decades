19ad1de5830b55d29961943b63f60e89e776db0d
jaime030690@gmail.com <jechvarria@cgsinc.com>
2020-02-05 13:26:12 -0500

    Created config file.

---
+++ config.py
@@ -0,0 +1,17 @@
+client_id = '05479fb61105417297085c5edea7627c'
+client_secret = '41f13c9d9a7b4eec8767256141414dc4'
+
+key_signatures = {
+    0: 'C',
+    1: 'C#/Db',
+    2: 'D',
+    3: 'Eb',
+    4: 'E',
+    5: 'F',
+    6: 'F#/Gb',
+    7: 'G',
+    8: 'Ab',
+    9: 'A',
+    10: 'Bb', 
+    11: 'B'
+}
