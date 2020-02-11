b87f66f9a05b9a86c19f594900301b0698c25c4a
Jaime Echevarria <jaime030690@gmail.com>
2020-02-10 23:11:48 -0500

    Added 2D histograms for years, key and bpm visualization.

---
+++ playlists.py
@@ -0,0 +1,10 @@
+# Dict to hold playlists.
+playlists = {
+    '50s': 'spotify:playlist:37i9dQZF1DWSV3Tk4GO2fq',
+    '60s': 'spotify:playlist:37i9dQZF1DXaKIA8E7WcJj',
+    '70s': 'spotify:playlist:37i9dQZF1DWTJ7xPn4vNaz',
+    '80s': 'spotify:playlist:37i9dQZF1DX4UtSsGT1Sbe',
+    '90s': 'spotify:playlist:37i9dQZF1DXbTxeAdrVG2l',
+    '00s': 'spotify:playlist:37i9dQZF1DX4o1oenSJRJd',
+    '10s': 'spotify:playlist:37i9dQZF1DX5Ejj0EkURtP'
+}
